44756969014a742a3f5998d3a743789e28ec703b
agranger13 <39957129+agranger13@users.noreply.github.com>
2022-07-04 11:32:16 +0200

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,42 +1,44 @@
 import json
 import pickle
-import boto3
+import boto3 
 import numpy
 
 s3 = boto3.resource('s3')
-s3.Bucket('spookydata').download_file('model/simple_QL_saved_water.pkl', '/tmp/simple_QL_saved_water.pkl')
-s3.Bucket('spookydata').download_file('model/simple_QL_saved_light.pkl', '/tmp/simple_QL_saved_light.pkl')
-
+s3.Bucket('spookydata').download_file('model/simple_QL_saved_water.pkl', '/tmp/simple_QL_saved_water.pkl') 
+s3.Bucket('spookydata').download_file('model/simple_QL_saved_light.pkl', '/tmp/simple_QL_saved_light.pkl') 
 
 def read_Q(name):
     with (open(f"/tmp/simple_QL_saved_{name}.pkl", "rb")) as openfile:
         Q_v = pickle.load(openfile)
     return Q_v
 
-
 def predict(value, type):
     Q_v = read_Q(type)
-    b_a = min(Q_v.keys(), key=lambda x: abs(float(x) - 0.05))
-    return max(Q_v[b_a])
-
-
+    print(value)
+    b_a = min(Q_v.keys(), key=lambda x:abs(float(x)-value))
+    print(Q_v[b_a])
+    return max(Q_v[b_a], key=Q_v[b_a].get)
+    #return max(Q_v[b_a])
+     
 def lambda_handler(event, context):
     payload = {
-        "water": False,
-        "light": False
+        "water" : "off",
+        "light" : "off"
     }
     action_w = predict(event["humidity_soil"], "water")
-    action_l = predict(event["luminosity"] / 100, "light")
-
-    if action_w == 0:
-        payload["water"] = False
-    else:
-        payload["water"] = True
-
-    if action_l == 0:
-        payload["light"] = False
-    else:
-        payload["light"] = True
+    action_l = predict(event["luminosity"]/100, "light")
+    
+    print(action_w, action_l)
+    
+    if action_w == "20" :
+        payload["water"] = "on"
+    else :
+        payload["water"] = "off"
+        
+    if action_l == "20" :
+        payload["light"] = "on"
+    else :
+        payload["light"] = "off"
 
     iot = boto3.client('iot-data')
     response = iot.publish(
